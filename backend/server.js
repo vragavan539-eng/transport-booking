359744beefd3a87876a1c8d2a881c5a57cbbe75e
vragavan539-eng <your@email.com>
2026-04-08 17:57:39 +0530

    Fix CORS with manual headers

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,15 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: '*',
-  credentials: false
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json());
 app.use(morgan('dev'));
 
